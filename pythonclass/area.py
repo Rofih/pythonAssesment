1e5e396b65818ee9d7551e2fe70767fe0ae4dffa
Akerele Rofih <rofihakerele@gmail.com>
2024-08-21 16:44:45 +0100

    initial commit

---
+++ pythonclass/area.py
@@ -0,0 +1,11 @@
+'''
+enter radius as 10
+enter pi as 3.142
+calculate,area
+multiply pi by radius and radius
+display area
+'''
+radius=10
+pi=3.142
+area=pi*radius*radius
+print(area)
---
+++ pythonclass/average.py
@@ -0,0 +1,22 @@
+'''
+request for the first number
+store as value1
+request for the second number
+store as value2
+request for the third number
+store as value3
+sum the number
+calculate,average
+divide the sum by 3
+display the average
+'''
+
+
+
+
+value1=int(input("Enter the first number: "))
+value2=int(input("Enter the second number: "))
+value3=int(input("Enter the second number: "))
+sum=value1+value2+value3
+average=sum/3
+print(average)
