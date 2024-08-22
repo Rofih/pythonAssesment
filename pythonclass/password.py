5f08b884946143b0b8eed2cb1e4c5a0364a04893
Akerele Rofih <rofihakerele@gmail.com>
2024-08-22 10:32:57 +0100

    password

---
+++ pythonclass/password.py
@@ -0,0 +1,12 @@
+'''
+request for a password
+store as input1
+count number of characters
+multiply the symbol* by number of characters
+display
+
+'''
+password = (input("Enter password: "))
+value=(len(password))
+pattern="*" *value
+print(pattern)
