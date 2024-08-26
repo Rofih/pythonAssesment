86308375c636f03302527adf4c4ed5e2259383fb
Akerele Rofih <rofihakerele@gmail.com>
2024-08-26 12:34:20 +0100

    second commit

--- pythonassignment/mortgage_calculator.py
+++ pythonassignment/mortgage_calculator.py
@@ -2,8 +2,10 @@ amount=int(input("Enter principal amount: "))
 rate=int(input("Enter the annual intrest rate: "))
 duration=int(input("Enter the duration in years: "))
 
+rate1=rate/100
+duration1=duration/12
 
-value1=int(rate*(1+rate)^duration)
-value2=int(((1+rate)^duration)-1)
+value1=int(rate1*(1+rate1)^duration1)
+value2=int(((1+rate1)^duration1)-1)
 mortgage=int(amount*(value1/value2))
 print("Your monthly payment is ",mortgage)
