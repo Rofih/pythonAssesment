ee9091e923ad1427dcbfb7c05af987095eff19f4
Akerele Rofih <rofihakerele@gmail.com>
2024-09-01 23:24:34 +0100

    initial

---
+++ pythonassignment/Logistic.py
@@ -0,0 +1,31 @@
+    
+value=int(input("Enter the number of deliveries:"))
+     
+       
+if(value<50):
+   int poor=(value*160)+5000
+   print(poor) 
+
+
+ higher=59
+ lower=50
+ if(value>lower and value<higher):
+    rich=(value*200)+5000
+    print(rich) 
+
+ high=69
+ low=60
+ if(value>low and value<high):
+   frosh=(value*250)+5000
+   print(frosh)
+
+
+ if(value>=70):
+   oga=(value*500)+5000
+   print(oga) 
+
+
+
+
+
+
