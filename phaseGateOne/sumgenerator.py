0a4c5e8f8b418022d51a611d7a2de8798fff4ea7
Akerele Rofih <rofihakerele@gmail.com>
2024-09-30 11:52:32 +0100

    first commit

---
+++ phaseGateOne/sumgenerator.py
@@ -0,0 +1,18 @@
+'''
+request for user input then store
+generate a random number then store
+generate another random number then store
+sum the the generated random numbers
+'''
+import random
+user_input=int(input("enter the sum of the random generated numbers:"))
+
+number=random.randrange(100)
+number2=random.randrange(100)
+
+add=number+number2
+
+if user_input==add:
+	print("true")
+else:
+	print("false")
